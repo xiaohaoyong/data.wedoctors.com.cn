efa526159c7f89b6273ed419c2470fee670d2315
wangzhen <wangzhen@xywy.com>
2017-11-08 16:31:38 +0800

    first

--- views/dy-comment/index.php
+++ views/dy-comment/index.php
@@ -9,44 +9,58 @@ use yii\grid\GridView;
 
 $this->title = '管理列表';
 $this->params['breadcrumbs'][] = $this->title;
-\app\components\helper\HeaderActionHelper::$action=[
-0=>['name'=>'添加','url'=>['create']]
-];
+
 ?>
 <div class="dy-comment-index">
 
-    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
-    <hr>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         
 'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
             'id',
-            'userid',
-            'touserid',
+            [
+                'attribute' => 'userid',
+                'value' => function($e)
+                {
+                    $user=\app\models\UserInfo::findOne($e->userid);
+
+                    return $user->name;
+                }
+            ],
+            [
+                'attribute' => 'touserid',
+                'value' => function($e)
+                {
+                    $user=\app\models\UserInfo::findOne($e->touserid);
+
+                    return $user->name;
+                }
+            ],
             'content',
-            'type',
+            [
+                'attribute' => 'dynamicid',
+                'value' => function($e)
+                {
+                    $dynamic=\app\models\dynamic\Dynamic::findOne($e->dynamicid);
+                    if($dynamic && $dynamic->source==5)
+                    {
+                        $dynamicRow=\app\models\dynamic\DyOpen::findOne(['dynamicid'=>$e->dynamicid]);
+                    }elseif($dynamic && $dynamic->source==4){
+                        $dynamicRow=\app\models\dynamic\DySeminar::findOne(['dynamicid'=>$e->dynamicid]);
+                    }
+
+                    return $dynamicRow?$dynamicRow->title:"";
+                }
+            ],
             // 'dynamicid',
             // 'level',
-            // 'createtime:datetime',
+            [
+                'attribute' => 'createtime',
+                'format' => ['date', 'php:Y-m-d H:i:s']
+            ],
             // 'pid',
             // 'source',
             // 'parentid',
-
-            [
-                'class' => 'app\components\grid\ActionColumn',
-                'template'=>'
-                <div class="btn-group dropup">
-                    <a class="btn btn-circle btn-default btn-sm" href="javascript:;" data-toggle="dropdown" aria-expanded="false">
-                        <i class="icon-settings"></i> 操作 <i class="fa fa-angle-up"></i></a>
-                    <ul class="dropdown-menu pull-right" role="menu">
-                        <li>{update} </li><li>{delete}</li>
-                    </ul>
-                </div>
-                ',
-            ],
         ],
     ]); ?>
 </div>
